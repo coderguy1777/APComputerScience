7a6b6ae5f6dc2f8138766cb9a64f7cb51064a178
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-01-24 19:09:15 -0500

    Add files via upload

--- Assignments/SentenceSorterHW/SentenceSorter.java
+++ Assignments/SentenceSorterHW/SentenceSorter.java
@@ -7,11 +7,11 @@ public class SentenceSorter {
         // Obtains the input for the program, for the string sorting input.
         Scanner scan = new Scanner(System.in);
         System.out.println("Hello, type a string to be sorted!");
-        String testy = scan.next();
+        String sortinput = scan.next();
 
-        ArrayList<Character> testsss = new ArrayList<>();
-        Map<Character, Integer>testmap = new HashMap<Character, Integer>();
-        char[]ch = new char[testy.length()];
+        ArrayList<Character> sentencearraylist = new ArrayList<>();
+        Map<Character, Integer>lettermap = new HashMap<Character, Integer>();
+        char[]ch = new char[sortinput.length()];
 
         // An integer meant for counting the occurences of each Character in the sentence added.
         int count = 0;
@@ -20,22 +20,22 @@ public class SentenceSorter {
         int[]test = new int[256];
 
         // Sets up the count for the counting array of the
-        for(int x = 0; x < testy.length(); x++) {
-            test[testy.toLowerCase().charAt(x)]++;
+        for(int x = 0; x < sortinput.length(); x++) {
+            test[sortinput.toLowerCase().charAt(x)]++;
         }
 
         // Nested for loop for counting the occurrence of each character
-        for (int i = 0; i < testy.length(); i++) {
-            ch[i] = testy.toLowerCase().charAt(i);
+        for (int i = 0; i < sortinput.length(); i++) {
+            ch[i] = sortinput.toLowerCase().charAt(i);
             for (int ii = 0; ii <= i; ii++) {
-                if (ch[i] == testy.toLowerCase().charAt(i)) {
+                if (ch[i] == sortinput.toLowerCase().charAt(i)) {
                     count++;
                 }
             }
-            testmap.put(testy.toLowerCase().charAt(i), test[testy.charAt(i)]);
+            lettermap.put(sortinput.toLowerCase().charAt(i), test[sortinput.charAt(i)]);
         }
 
-        Set<Map.Entry<Character, Integer>>testset = testmap.entrySet();
+        Set<Map.Entry<Character, Integer>>testset = lettermap.entrySet();
         System.out.println("Letter Occurrences:");
 
         // Prints the Occurrences of each letter in the sentence
@@ -43,16 +43,16 @@ public class SentenceSorter {
             System.out.println(me.getKey() + ":" + me.getValue());
         }
         // Converts the Inputted String into all lowercase letters to ignore cases.
-        char[]testlistt = testy.toCharArray();
-        Arrays.sort(testlistt);
+        char[]sentencechars = sortinput.toCharArray();
+        Arrays.sort(sentencechars);
 
         //Adds the sorted char array to an arraylist, so it can be printed out all in one line for the final sort.
-        for(int i = 0; i < testlistt.length; i++) {
-            testsss.add(testlistt[i]);
+        for(int i = 0; i < sentencechars.length; i++) {
+            sentencearraylist.add(sentencechars[i]);
         }
 
         //Prints Sorted Sentence, with Punctuation at the end.
         System.out.println("-------------------------------------------------");
-        System.out.println("Sorted sentence: " + testsss);
+        System.out.println("Sorted sentence: " + sentencearraylist);
     }
 }
