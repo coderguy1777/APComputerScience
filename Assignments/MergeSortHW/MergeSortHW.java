f3c61679fd253c20c6bf11c63d5d1c627a26da51
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-01-30 14:10:42 -0500

    Add files via upload

--- Assignments/MergeSortHW/MergeSortHW.java
+++ Assignments/MergeSortHW/MergeSortHW.java
@@ -1,6 +1,3 @@
-package MergeSortHW;
-package test;
-
 import java.io.*;
 import java.util.*;
 
@@ -15,14 +12,14 @@ public class MergeSortHW {
         try {
             scan = new Scanner(file);
             while(scan.hasNextLine()) {
-                inputarr.add(scan.nextInt());
+                inputarr.add(Integer.valueOf(scan.nextLine()));
             }
 
         } catch (IOException e) {
             System.out.print(e);
         }
         mergeysort(inputarr);
-        System.out.println(sortarr);
+        System.out.println(inputarr);
     }
 
     public static void mergeysort(ArrayList<Integer>inputus) {
@@ -32,28 +29,43 @@ public class MergeSortHW {
             if (count == inputus.size() / 2) {
                 List<Integer> half2tree = inputus.subList(inputus.size() / 2, inputus.size());
                 List<Integer> half1tree = inputus.subList(0, inputus.size() / 2);
-                int[] half1 = new int[half1tree.size()];
-                int[] half2 = new int[half2tree.size()];
-                for (int x = 0; x < half1tree.size(); x++) {
-                    int xx = half1tree.get(x);
-                    half1[x] = xx;
-                }
 
-                for (int g = 0; g < half2tree.size(); g++) {
-                    int gg = half2tree.get(g);
-                    half2[g] = gg;
-                }
-                for (int z = 0; z < half1.length; z++) {
-                    for (int z2 = z; z2 < half1.length; z2 = z + 1) {
-                        if (half1[z] != half1[z2]) {
-                            half1[z] = half1[z + 1];
+
+                for (int z = 0; z < half1tree.size(); z++) {
+                    for (int z2 = z; z2 < half1tree.size(); z2++) {
+                        if (half1tree.get(z).compareTo(half1tree.get(z2)) > 0) {
+                            List<Integer> ic = half1tree.subList(0, half1tree.size()/2);
+                            List<Integer> iu = half1tree.subList(half1tree.size()/2, half1tree.size());
                             int half11 = z;
-                            half1tree.set(half11, half1[z + 1]);
-                            System.out.println(half1tree);
-                            break;
-                        }
-                        for (int v = 0; v < half1tree.size(); v++) {
-                            System.out.println(half1tree.get(v));
+                            half1tree.set(z, half1tree.get(z));
+                            System.out.println(ic);
+                            System.out.println(iu);
+
+                            if(ic.size() % 2 == 0) {
+                                List<Integer>ic4 = ic.subList(0, ic.size()/2);
+                                List<Integer>ic5 = ic.subList(ic.size()/2 , ic.size());
+
+
+                            }
+
+                            if(iu.size() % 2 == 0) {
+                                List<Integer>iu4 = iu.subList(iu.size() - iu.size(), iu.size()/2);
+                                List<Integer>iu5 = iu.subList(iu.size()/2, iu.size());
+
+                                if(iu4.size() % 2 == 0) {
+                                    List<Integer>finalu = iu4.subList(iu4.size() - iu4.size(), iu4.size() - 2);
+                                    List<Integer>finau2 = iu4.subList(iu4.size() - 2, iu4.size());
+                                    System.out.println(finalu);
+                                    System.out.println(finau2);
+                                }
+
+                                if(iu5.size() % 2 == 0) {
+                                    List<Integer>finalc = iu5.subList(iu5.size() - iu5.size(), iu5.size() - 2);
+                                    List<Integer>finalc2 = iu5.subList(iu5.size() - 2,  iu5.size());
+                                    System.out.println(finalc);
+                                    System.out.println(finalc2);
+                                }
+                            }
                         }
                     }
                 }
