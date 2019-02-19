90fa7dcf3a6e60b1766df203042dab7aeb27fb00
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-02-18 20:29:29 -0500

    Add files via upload

--- Assignments/MatrixProject/MatrixTest.java
+++ Assignments/MatrixProject/MatrixTest.java
@@ -3,13 +3,13 @@ public class MatrixTest {
     public static void main(String[]args) {
         try {
             Matrix a = new Matrix(2,3);
-            a.setmatrixvals(0, 0, 2.1);
-            a.setmatrixvals(0, 1, 1.2);
-            a.setmatrixvals(1,0, 1);
-            Matrix b = new Matrix(3,3);
-            b.setmatrixvals(0,0, 2.1);
-            b.setmatrixvals(0, 1, 1.2);
-            b.setmatrixvals(1, 0, 1);
+            a.set(0, 0, 2.1);
+            a.set(0, 1, 1.2);
+            a.set(1,0, 1);
+            Matrix b = new Matrix(2,3);
+            b.set(0,0, 2.1);
+            b.set(0, 1, 1.2);
+            b.set(1, 0, 1);
             Matrix.mult(a, b);
         } catch (MatrixException e) {
             System.out.println(e.getMessage());
